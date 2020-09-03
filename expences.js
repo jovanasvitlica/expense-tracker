7bfefc116f0e6016f352b3fe165f25454099f252
Jovana Svitlica <svitlica3@gmail.com>
2020-09-03 18:41:02 +0200

    local storage

--- expences.js
+++ expences.js
@@ -8,12 +8,43 @@ const amount = document.getElementById("amount");
 
 const dataTransaction = [
   { id: 1, text: "Book", amount: -20 },
-  { id: 1, text: "Sunglases", amount: 100 },
+  { id: 1, text: "Sunglases", amount: 50 },
   { id: 1, text: "Table", amount: -50 },
-  { id: 1, text: "Food", amount: 60 },
+  { id: 1, text: "Food", amount: 40 },
 ];
-let transactions = dataTransaction;
-//funkcija koja dodajte transakcije na listu
+
+const localStorageTransactions = JSON.parse(
+  localStorage.getItem("transactions")
+);
+
+let transactions =
+  localStorage.getItem("transactions") !== null ? localStorageTransactions : [];
+
+//add transaction
+function addTransaction(e) {
+  e.preventDefault();
+
+  if (text.value.trim() === "" || amount.value.trim() === "") {
+    alert("Please add a text and amount");
+  } else {
+    const transaction = {
+      id: generateID(),
+      text: text.value,
+      amount: +amount.value,
+    };
+    transactions.push(transaction);
+    addTransactionDOM(transaction);
+    updateValues();
+    updateLocalStorage();
+    text.value = "";
+    amount.value = "";
+  }
+}
+//random ID
+function generateID() {
+  return Math.floor(Math.random() * 10000000);
+}
+//funkcija koja dodajte transakcije na DOM
 function addTransactionDOM(transaction) {
   // da li je u plusu ili minusu iznos
   const sign = transaction.amount < 0 ? "-" : "+";
@@ -26,7 +57,9 @@ function addTransactionDOM(transaction) {
     ${transaction.text} <span>${sign}$${Math.abs(transaction.amount).toFixed(
     2
   )}</span> 
-    <button class="delete-btn">x</button>
+    <button class="delete-btn" onclick="removeTransaction(${
+      transaction.id
+    })">x</button>
     `;
   list.appendChild(item);
 }
@@ -45,11 +78,22 @@ function updateValues() {
     amounts.filter((item) => item < 0).reduce((acc, item) => (acc += item), 0) *
     -1
   ).toFixed(2);
+
   balance.innerText = `$${total}`;
   money_plus.innerText = `$${income}`;
   money_minus.innerText = `$${expense}`;
 }
+//remove transaction by id
+function removeTransaction(id) {
+  transactions = transactions.filter((transaction) => transaction.id !== id);
 
+  updateLocalStorage();
+  init();
+}
+//update local storage
+function updateLocalStorage() {
+  localStorage.setItem("transactions", JSON.stringify(transactions));
+}
 //pozivanje liste
 function init() {
   list.innerHTML = "";
@@ -58,3 +102,4 @@ function init() {
   updateValues();
 }
 init();
+form.addEventListener("submit", addTransaction);
